b3d68dad1694971c68e0d9c30e8823e443275879
rationalcode <marat_pochta@mail.ru>
2018-02-06 17:36:55 +0500

    11 commit

--- app/src/main/java/com/example/admin/hist3/SelectActivity.java
+++ app/src/main/java/com/example/admin/hist3/SelectActivity.java
@@ -12,10 +12,13 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static com.example.admin.hist3.DatabaseChapter.getInstance;
+
 public class SelectActivity extends AppCompatActivity {
 
     static Fragment fragment;
 
+    Context context;
     public static Toast toast;
     String LOG_TAG = "fragment_state";
     private FragmentManager fm = getFragmentManager();
@@ -42,16 +45,17 @@ public class SelectActivity extends AppCompatActivity {
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
         Log.d(LOG_TAG, "fragment"+fragment.hashCode());
         }
-
-
-
-//
-//                DatabaseChapter db = Room.databaseBuilder(getApplicationContext(),
-//                DatabaseChapter.class, "chapter-database").build();
-
+        getInstance(getApplicationContext());
 
     }
 
 
+//    private DatabaseChapter createDB(){
+//
+//        db = Room.databaseBuilder(getApplicationContext(),
+//                DatabaseChapter.class, "chapter-database").build();
+//
+//        return db;
+//    }
 }
 
